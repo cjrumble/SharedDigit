9c203bb97dea5ef7b5fda69c4538a23a34a2948c
Chris <cjrumble@yahoo.com>
2021-02-17 17:25:56 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String args[]) {
+
+        System.out.println(SharedDigit.hasSharedDigit(99, 67));
+        System.out.println(SharedDigit.hasSharedDigit(12, 24));
+        System.out.println(SharedDigit.hasSharedDigit(10, 20));
+        System.out.println(SharedDigit.hasSharedDigit(44, 88));
+        System.out.println(SharedDigit.hasSharedDigit(32, 32));
+        System.out.println(SharedDigit.hasSharedDigit(9, 99));
+        System.out.println(SharedDigit.hasSharedDigit(99, 100));
+
+    }
+
+}
